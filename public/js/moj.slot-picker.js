d0fc4f0d54c56d13e627943468c2120bfbe1bc5c
Marzena Sokolowska <marzena.sokolowska@btfinancialgroup.com>
2015-11-18 14:54:23 +1100

    Display info about currently booked appointment if reschedule. Small calendar style fixes.

--- public/js/moj.slot-picker.js
+++ public/js/moj.slot-picker.js
@@ -653,3 +653,20 @@
   };
 
 }());
+
+
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+$(window).bind("load", function() { 
+    var rescheduled = getParameterByName("resch");
+    if("true"==rescheduled){
+        document.getElementById("currentAppDet").style.display = 'block';
+    }
+});
+
