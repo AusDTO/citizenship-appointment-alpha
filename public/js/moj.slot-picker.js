a596b015d472a8ec052df3dde21c74d390df3d08
Emily Siow <emily.siow@digital.gov.au>
2015-11-18 15:10:15 +1100

    Amend selector to increment/decrement nudgeNav once at a time

--- public/js/moj.slot-picker.js
+++ public/js/moj.slot-picker.js
@@ -97,12 +97,12 @@
         self.$timeSlots.addClass('is-active');
       });
 
-      this.$_el.on('click', '.BookingCalendar-nav--next', function(e) {
+      this.$_el.on('click', 'button.BookingCalendar-nav--next', function(e) {
         e.preventDefault();
         self.nudgeNav(1);
       });
 
-      this.$_el.on('click', '.BookingCalendar-nav--prev', function(e) {
+      this.$_el.on('click', 'button.BookingCalendar-nav--prev', function(e) {
         e.preventDefault();
         self.nudgeNav(-1);
       });
