94058e73afa05cad9cb32957ac71a07eb3714f72
Halvard Skogsrud <halvard@skogsrud.com>
2015-11-25 16:48:19 +1100

    Added livereload support

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     consolidate = require('consolidate'),
     hogan = require('hogan.js'),
     uuid = require('node-uuid'),
-    qr = require('qr-image'), 
+    qr = require('qr-image'),
     path = require('path'),
     querystring = require('querystring'),
     default_port = (process.env.PORT || 3000);
@@ -11,6 +11,9 @@ var express = require('express'),
 app.engine('mustache', consolidate.hogan); // set templating engine for .mustache files
 app.set('view engine', 'mustache');  // default extension for template names when used with res.render
 app.set('views', path.join(__dirname, 'templates')); // default template location
+
+app.use(require('connect-livereload')());  // runs livereload server and serves livereload.js
+require('express-livereload')(app);  // inserts <script> reference to livereload.js
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -36,7 +39,7 @@ app.get('/googlecalendar', function(req, res) {
 
 app.get('/qrcode', function(req, res) {  
   var code = qr.image(
-    "Welcome to our awesome Alpha! Your client id is: 9281112121", 
+    "Welcome to our awesome Alpha! Your client id is: 9281112121",
     { type: 'svg' }
   );
   res.type('svg');
